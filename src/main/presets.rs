4ab0cf4b442b92530f4e8e1b49dd78347fedd790
Adrian John <adrianjohn6543@gmail.com>
2023-12-24 14:15:26 +0100

    add even more presets

--- src/main/presets.rs
+++ src/main/presets.rs
@@ -1,8 +1,10 @@
-use wjp::Values;
+use wbdl::Date;
+use wjp::{Serialize, Values};
 
 use crate::method::WIMCMethods;
 use crate::out::WIMCOutput;
 use crate::r#in::WIMCInput;
+use crate::WIMCError;
 
 const SPACE: char = ' ';
 const CLEANUP: WIMCInput = WIMCInput::from_val(Values::Null, Vec::new(), WIMCMethods::Cleanup);
@@ -39,3 +41,29 @@ pub const fn cleanup() -> WIMCInput {
     CLEANUP
 }
 
+pub fn store<T: Serialize>(obj: T, mut params: Vec<String>, time: Option<Date>) -> WIMCInput {
+    if let Some(time) = time {
+        params.push(time.to_string())
+    }
+    WIMCInput::new(obj, params, WIMCMethods::Store)
+}
+
+pub fn stored(id: usize) -> WIMCOutput {
+    WIMCOutput::from_values(Values::Number(id as f64))
+}
+
+pub fn error(error: WIMCError) -> WIMCOutput {
+    WIMCOutput::from(error)
+}
+
+pub fn found(values: Values) -> WIMCOutput {
+    WIMCOutput::from_values(values)
+}
+
+pub fn get(id: usize) -> WIMCInput {
+    WIMCInput::from_val(Values::Null, vec![id.to_string()], WIMCMethods::Get)
+}
+
+pub fn query(params: Vec<String>) -> WIMCInput {
+    WIMCInput::from_val(Values::Null, params, WIMCMethods::Query)
+}
