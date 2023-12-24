bc932f0ae7c8256889211b1533a258aa94753c4a
Adrian John <adrianjohn6543@gmail.com>
2023-12-24 13:11:33 +0100

    make it ser and deserializable

--- src/main/in.rs
+++ src/main/in.rs
@@ -1,7 +1,28 @@
+use wjp::{map, ParseError, Serialize, SerializeHelper, Values};
+
 use crate::method::WIMCMethods;
 
 pub struct WIMCInput {
     payload: String,
     params: Vec<String>,
     method: WIMCMethods,
+}
+
+impl Serialize for WIMCInput {
+    fn serialize(&self) -> Values {
+        Values::Struct(map!(
+            ("payload", self.payload.serialize()),
+            ("params", self.params.serialize()),
+            ("method", self.method.serialize())
+        ))
+    }
+}
+impl TryFrom<Values> for WIMCInput {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        let mut struc = value.get_struct().ok_or(ParseError::new())?;
+        let payload = struc.map_val("payload", String::try_from)?;
+        let params = struc.map_val("params", Vec::try_from)?;
+        let method = struc.map_val("method", WIMCMethods::try_from)?;
+    }
 }
