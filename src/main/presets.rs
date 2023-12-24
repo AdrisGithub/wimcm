dc5cdabc92c518f159334f0e983261eb0ce630d4
Adrian John <adrianjohn6543@gmail.com>
2023-12-24 14:03:56 +0100

    fix presets

--- src/main/presets.rs
+++ src/main/presets.rs
@@ -18,13 +18,18 @@ pub const fn ping() -> WIMCInput {
 }
 
 pub fn pong() -> WIMCOutput {
-    WIMCOutput::default()
+    respond(vec![String::from("Pong")])
 }
 
 pub fn respond(msg: Vec<String>) -> WIMCOutput {
     let mut string = String::new();
+    let mut first = true;
     for word in msg {
-        string.push(SPACE);
+        if first {
+            first = false;
+        } else {
+            string.push(SPACE);
+        }
         string.push_str(word.as_str());
     };
     WIMCOutput::from_values(Values::String(string))
