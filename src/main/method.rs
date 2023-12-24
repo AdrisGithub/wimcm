b187b6e420544ea9d72d5f6d3be8078b425dbe6b
Adrian John <adrianjohn6543@gmail.com>
2023-12-24 13:14:46 +0100

    add some tryfroms for method

--- src/main/method.rs
+++ src/main/method.rs
@@ -1,6 +1,11 @@
 use std::fmt::{Debug, Display, Formatter};
+use std::str::FromStr;
 
-use wjp::{Serialize, Values};
+use wjp::{ParseError, Serialize, Values};
+
+use crate::error::WIMCError;
+
+const NAMES: [&str; 3] = ["Ping", "Echo", "Cleanup"];
 
 #[derive(Copy, Clone, PartialOrd, PartialEq, Ord, Eq, Hash, Default, Debug)]
 pub enum WIMCMethods {
@@ -19,5 +24,43 @@ impl Serialize for WIMCMethods {
 impl Display for WIMCMethods {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         Debug::fmt(self, f)
+    }
+}
+
+impl TryFrom<Values> for WIMCMethods {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        Self::try_from(value.get_string().ok_or(ParseError::new())?)
+            .map_err(|err| ParseError::new())
+    }
+}
+
+impl FromStr for WIMCMethods {
+    type Err = WIMCError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        NAMES
+            .iter()
+            .position(|&name| name.eq_ignore_ascii_case(s))
+            .map(WIMCMethods::try_from)
+            .ok_or(WIMCError)?
+    }
+}
+
+impl TryFrom<String> for WIMCMethods {
+    type Error = WIMCError;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Self::from_str(value.as_str())
+    }
+}
+
+impl TryFrom<usize> for WIMCMethods {
+    type Error = WIMCError;
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(WIMCMethods::Ping),
+            1 => Ok(WIMCMethods::Echo),
+            2 => Ok(WIMCMethods::Cleanup),
+            _ => Err(WIMCError),
+        }
     }
 }
