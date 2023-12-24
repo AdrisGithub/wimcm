83d6e87f0ccee27731694817a09fa202726a7437
Adrian John <adrianjohn6543@gmail.com>
2023-12-24 12:51:13 +0100

    add Serialize for WIMCMethods

--- src/main/method.rs
+++ src/main/method.rs
@@ -1,7 +1,23 @@
-#[derive(Copy, Clone, PartialOrd, PartialEq, Ord, Eq, Hash, Debug,Default)]
+use std::fmt::{Debug, Display, Formatter};
+
+use wjp::{Serialize, Values};
+
+#[derive(Copy, Clone, PartialOrd, PartialEq, Ord, Eq, Hash, Default, Debug)]
 pub enum WIMCMethods {
     #[default]
     Ping,
     Echo,
     Cleanup,
+}
+
+impl Serialize for WIMCMethods {
+    fn serialize(&self) -> Values {
+        Values::String(self.to_string())
+    }
+}
+
+impl Display for WIMCMethods {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(self, f)
+    }
 }
