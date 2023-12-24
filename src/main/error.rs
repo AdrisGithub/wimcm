6574e11b41049f7ce74d86c93f4b36247c37cff1
Adrian John <adrianjohn6543@gmail.com>
2023-12-24 12:44:07 +0100

    make error serializable

--- src/main/error.rs
+++ src/main/error.rs
@@ -1,10 +1,26 @@
 use std::num::ParseIntError;
 
-#[derive(Debug, Default)]
+use wjp::{ParseError, Serialize, Values};
+
+#[derive(Debug, Default,Copy, Clone)]
 pub struct WIMCError;
 
 impl From<ParseIntError> for WIMCError {
     fn from(_value: ParseIntError) -> Self {
         Self
+    }
+}
+
+impl Serialize for WIMCError {
+    fn serialize(&self) -> Values {
+        Values::String(String::from("Hello"))
+    }
+}
+
+impl TryFrom<Values> for WIMCError {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        let string = value.get_string().ok_or(ParseError::new())?;
+        Ok(WIMCError)
     }
 }
